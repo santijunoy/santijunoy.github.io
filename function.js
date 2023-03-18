0cac167e881d39742af184aa7bb73639bd89b00f
santijunoy <125909823+santijunoy@users.noreply.github.com>
2023-03-18 18:29:01 -0300

    First commit

---
+++ function.js
@@ -0,0 +1,8 @@
+
+document.getElementById('agregar_color').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green' ;
+})
+
+document.getElementById('quitar_color').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white' ;
+})
